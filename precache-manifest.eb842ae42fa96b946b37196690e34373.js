49ab50db19b39910e42abeb8056074c85124a93a
cjalex1313 <cioarec.alexandru96@gmail.com>
2021-04-06 09:14:24 +0000

    Update from https://github.com/AdiCutitoiu/MeditatiiClient/commit/e330fe7671496805635c00ff19afa57d669e5aeb

---
+++ precache-manifest.eb842ae42fa96b946b37196690e34373.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37c754ca6dcc50299b5",
+    "url": "/css/app.9af4f3ad.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ca03baf6fa16a2856736ffb6635e189f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd71dd925d5ba8573d5",
+    "url": "/js/about.737681ec.js"
+  },
+  {
+    "revision": "b37c754ca6dcc50299b5",
+    "url": "/js/app.5c408420.js"
+  },
+  {
+    "revision": "9273edad1fa227faa70a",
+    "url": "/js/chunk-vendors.a35350a1.js"
+  },
+  {
+    "revision": "12a009c31f1bf291dacf9f0533393840",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
